76b9df28159bea353801277448a9ce13ae1429cf
GKTNgames <guilhermektaba@gmail.com>
2026-05-05 14:31:53 -0300

    fix: imports

--- filmedle/src/components/modoInfinito/modoInfinito.jsx
+++ filmedle/src/components/modoInfinito/modoInfinito.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './modoInfinito.css'
-import CinemaBackground from "../cinemaBackground/cinemaBackground.jsx";
+import CinemaBackground from "../cinemaBackground/CinemaBackground.jsx";
 
 const BASE_URL = '/api'
 
