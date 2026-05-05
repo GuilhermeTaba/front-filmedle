6f5d2ea0ca808383365695f7d4b2fdd5ee68c301
GKTNgames <guilhermektaba@gmail.com>
2026-05-05 14:20:35 -0300

    fix: imports

--- filmedle/src/main.jsx
+++ filmedle/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './components/landingPage/landingPage'
+import LandingPage from './components/landingPage/landingPage.jsx'
 import ModoDiario from './components/modoDiario/modoDiario.jsx'
 import ModoInfinito from './components/modoInfinito/modoInfinito.jsx'
 import './index.css'  
